654c696c172d0681dd49f290470f1cc24ed74127
Walter Whitman <GitHub@TheJoyfulProgrammer.com>
2019-03-12 17:09:23 -0400

    Tidied up code and added comment header.

--- Count-Character-Types-In-String-Test-v1.cpp
+++ Count-Character-Types-In-String-Test-v1.cpp
@@ -1,3 +1,21 @@
+/*
+    Written by:  Walter Whitman aka Waltersmind aka The Joyful Programmer
+    March 4th, 2018
+    My website: http://www.TheJoyfulProgrammer.com/
+    FaceBook:   https://www.facebook.com/thejoyfulprogrammer/
+    Twitter:  	https://twitter.com/TheJoyfulProg
+    YouTube:    https://www.youtube.com/thejoyfulprogrammer
+    Pinterest:  https://www.pinterest.com/waltersmind/
+    GitHub:     https://github.com/TheJoyfulProgrammer/
+    Programming Language:     C++
+    Tested Compilers:         G++ ver 8, MingW W64 GCC-8.1.0 x86_64-win32-seh
+    Tested Operating Systems: Linux Mint 19, Windows 7, Windows 10
+    Description: This C++ demo asks for a message from the user and the displays how many
+                 consonants, vowels, number, spaces, and punctuations there are in the
+                 message.
+
+*/
+
 #include <iostream>
 
 int main()
@@ -17,16 +35,10 @@ int main()
 
     for (Iterator1 = 0; Iterator1 < (int)UserInput.length(); Iterator1++)
     {
-        if (isalpha(UserInput[Iterator1]))
-        {
-            vowels.find(tolower(UserInput[Iterator1])) != std::string::npos ? CounterVowels++ : CounterConsonants++;
-        } else if (isdigit(UserInput[Iterator1])) {
-            CounterNumbers++;
-        } else if (isspace(UserInput[Iterator1])) {
-            CounterSpaces++;
-        } else if (ispunct(UserInput[Iterator1])) {
-            CounterPunctuation++;
-        }
+        if (isalpha(UserInput[Iterator1])) vowels.find(tolower(UserInput[Iterator1])) != std::string::npos ? CounterVowels++ : CounterConsonants++;
+        if (isdigit(UserInput[Iterator1])) CounterNumbers++;
+        if (isspace(UserInput[Iterator1])) CounterSpaces++;
+        if (ispunct(UserInput[Iterator1])) CounterPunctuation++;
     }
 
     std::cout << "\n\nConsonants: " << CounterConsonants << "\n";
