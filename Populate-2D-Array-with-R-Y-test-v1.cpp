75393aa8b9fa69e04f363a6d238d2bf68b7845f1
TheJoyfulProgrammer <GitHub@TheJoyfulProgrammer.com>
2019-03-20 20:44:23 -0400

    NEW DEMO

---
+++ Populate-2D-Array-with-R-Y-test-v1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+
+namespace ConnectFour
+{
+    const uint8_t BoardRows{6};
+    const uint8_t BoardColumns{7};
+
+    char Board[BoardRows][BoardColumns];
+    int BoardTracker[BoardRows * BoardColumns];
+    uint16_t CurrentPosition{0};
+
+    std::string PopulationCharacters = "RY";
+
+    void ResetBoard()
+    {
+        for (uint16_t Iterator1 = 0; Iterator1 < (BoardRows * BoardColumns); Iterator1++)
+        {
+            Board[Iterator1 / BoardColumns][Iterator1 % BoardColumns] = '.';
+            BoardTracker[Iterator1] = Iterator1;
+            CurrentPosition = 0;
+        }
+
+        srand (time(NULL));
+    }
+
+    bool populateBoard()
+    {
+        uint16_t RandomCell = (rand() % ((BoardRows * BoardColumns) - CurrentPosition));
+        std::swap(BoardTracker[CurrentPosition], BoardTracker[RandomCell + CurrentPosition]);
+
+        uint16_t NewPosition = BoardTracker[CurrentPosition];
+
+        Board[NewPosition / BoardColumns][NewPosition % BoardColumns] = PopulationCharacters[rand() % PopulationCharacters.length()];
+
+        CurrentPosition++;
+        if (CurrentPosition == (BoardRows * BoardColumns )) return true;
+
+        return false;
+    }
+
+    void printBoard()
+    {
+        std::cout << "\n        Current Iteration: " << CurrentPosition << "\n";
+
+        for (uint16_t Iterator1 = 0; Iterator1 <= (BoardRows * BoardColumns); Iterator1++)
+        {
+            if (Iterator1 % BoardColumns == 0) std::cout << "\n        ";
+            std::cout << Board[Iterator1 / BoardColumns][Iterator1 % BoardColumns];
+        }
+
+        std::cout << "\n";
+    }
+}
+
+int main()
+{
+    bool ReturnValue;
+
+    ConnectFour::ResetBoard();
+
+    do
+    {
+        ReturnValue = ConnectFour::populateBoard();
+        ConnectFour::printBoard();
+    } while (ReturnValue == false);
+
+    return 0;
+}
