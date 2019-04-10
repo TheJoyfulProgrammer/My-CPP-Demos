c9ffb33c39e71bac304eeef6774b3a73f804549c
Walter Whitman <GitHub@TheJoyfulProgrammer.com>
2019-04-09 23:29:32 -0400

    Add header comments

--- Guess-A-Numer-Computer-AI-Test-v1.cpp
+++ Guess-A-Numer-Computer-AI-Test-v1.cpp
@@ -1,3 +1,18 @@
+/*
+    Written by:  Walter Whitman aka Waltersmind aka The Joyful Programmer
+    April 9th, 2019
+    My website: http://www.TheJoyfulProgrammer.com/
+    FaceBook:   https://www.facebook.com/thejoyfulprogrammer/
+    Twitter:  	https://twitter.com/TheJoyfulProg
+    YouTube:    https://www.youtube.com/thejoyfulprogrammer
+    Pinterest:  https://www.pinterest.com/waltersmind/
+    GitHub:     https://github.com/TheJoyfulProgrammer/
+    Programming Language:     C++
+    Tested Compilers:         G++ ver 8, MingW W64 GCC-8.1.0 x86_64-win32-seh
+    Tested Operating Systems: Linux Mint 19, Windows 7, Windows 10
+    Description: This is a guess-a-number game where the computer guesses your number.
+*/
+
 #include <iostream>
 #include <cmath>
 #include <algorithm>
