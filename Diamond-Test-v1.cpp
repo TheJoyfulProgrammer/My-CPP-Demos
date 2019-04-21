6d7906b268a86cc12217f4b53c684c2fed5a077e
Walter Whitman <GitHub@TheJoyfulProgrammer.com>
2019-04-21 14:58:29 -0400

    Added header comments

--- Diamond-Test-v1.cpp
+++ Diamond-Test-v1.cpp
@@ -1,3 +1,21 @@
+/*
+    Written by:  Walter Whitman aka Waltersmind aka The Joyful Programmer
+    April 14, 2019
+    My website: http://www.TheJoyfulProgrammer.com/
+    FaceBook:   https://www.facebook.com/thejoyfulprogrammer/
+    Twitter:  	https://twitter.com/TheJoyfulProg
+    YouTube:    https://www.youtube.com/thejoyfulprogrammer
+    Pinterest:  https://www.pinterest.com/waltersmind/
+    GitHub:     https://github.com/TheJoyfulProgrammer/
+    Programming Language:     C++ (ver 17)
+    Tested Compilers:         G++ ver 8, MingW W64 GCC-8.1.0 x86_64-win32-seh
+    Tested Operating Systems: Linux Mint 19, Windows 7, Windows 10
+    Description: This creates a diamond pattern. Since this is not using the
+                 <iomanip> library, we have to spread the stream settings into
+                 multiple std::cout lines using the "fill" and "width" functions
+                 of the std::cout function.
+*/
+
 #include <iostream>
 
 int main ()
