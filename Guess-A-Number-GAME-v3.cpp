a608555500221d926c827271a0793747e0c04555
Walter Whitman <GitHub@TheJoyfulProgrammer.com>
2020-04-28 05:57:13 -0400

    Modified the code a bit
    Cleaned up the variable declarations. Added initializing values to declarations. Resized the std::cout statements to be multiline and to fit GitHub's page. Added the usage of the <limits> library for clearing the stream. Replaced some IF statements with ternary statements, and moved them inside of the std::cout statements themselves. Removed the "return 0;" statement at the end, since it is not required in the main() function. Added a couple of comments (Yuck!!!).

--- Guess-A-Number-GAME-v3.cpp
+++ Guess-A-Number-GAME-v3.cpp
@@ -16,31 +16,49 @@
     Description: This is a Guess-A-Number style game, but with a twist, and personality.
 */
 
-#include <iostream>
 #include <ctime>
+#include <iostream>
+#include <limits>
 #include <vector>
 
 int main()
 {
-   const int MinNumber = 1;
-   const int MaxNumber = 20;
-   const int MaxGuessesLeft = 5;
+   const int MinNumber{1};
+   const int MaxNumber{20};
+   const int MaxGuessesLeft{5};
 
-   long long unsigned int iteration;
-   float ChancePercentageOfGuessingCorrectly;
-   int SecretNumber, GuessesLeft = MaxGuessesLeft, UserGuessedNumber, CinStatus;
-   unsigned int RandomMessage;
+   bool UserAlreadyGuessedThisNumber{0};
+   
+   int SecretNumber{0};
+   int GuessesLeft = MaxGuessesLeft;
+   int UserGuessedNumber{0};
+   int CinStatus{0};
+   
+   unsigned int RandomMessage{0};
+   long long unsigned int iteration{0};
+   
+   float ChancePercentageOfGuessingCorrectly{0};
+   
+   
+   
    std::vector<int> UserGuesses;
-   bool UserAlreadyGuessedThisNumber;
 
    std::srand(time(NULL));
 
    ChancePercentageOfGuessingCorrectly = ((float)MaxGuessesLeft / ((float)MaxNumber - (float)MinNumber + 1)) * 100;
    SecretNumber = std::rand() % MaxNumber + MinNumber;
 
-   std::cout << "###  I am thinking of a number from " << MinNumber << " to " << MaxNumber << ".\n###  I am giving you " << MaxGuessesLeft << " chance";
-   if(MaxGuessesLeft > 1) std::cout << "s";
-   std::cout << " to guess it.\n###  This means you have a " << ChancePercentageOfGuessingCorrectly << "% chance of guessing my number.\n\n\n";
+   std::cout << "###  I am thinking of a number from " 
+			 << MinNumber 
+			 << " to " 
+			 << MaxNumber 
+			 << ".\n###  I am giving you " 
+			 << MaxGuessesLeft 
+			 << " chance"
+			 << (MaxGuessesLeft > 1 ? "s" : "")
+			 << " to guess it.\n###  This means you have a " 
+			 << ChancePercentageOfGuessingCorrectly 
+			 << "% chance of guessing my number.\n\n\n";
 
    do
    {
@@ -58,27 +76,41 @@ int main()
                   if (std::cin.fail()) {
                       CinStatus = false;
 
-                      std::cin.clear();
-                      while ((getchar()) != '\n');
+   				      // Clear the std::cin error flag
+				      std::cin.clear();
+				      // Clear the remaining stream buffer 
+				      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
 
-                      std::cout << "\n\n###--->  I'm not sure what that answer means, but I know it's not a number\n###--->  from " << MinNumber << " to " << MaxNumber << ".\n\n\n";
+                      std::cout << "\n\n###--->  I'm not sure what that answer means, but I know it's not a number\n###--->  "
+								   "from " 
+								<< MinNumber 
+								<< " to " 
+								<< MaxNumber 
+								<< ".\n\n\n";
                   }
 
                } while (!CinStatus);
 
-               std::cin.clear();
-               while ((getchar()) != '\n');
+			   // Clear the remaining stream buffer 
+               std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
 
-               if((UserGuessedNumber < MinNumber) || (UserGuessedNumber > MaxNumber))
+               if((UserGuessedNumber < MinNumber) or (UserGuessedNumber > MaxNumber))
                {
                    std::cout << "\n\n";
 
                    if(UserGuessedNumber < 0)
                    {
-                       std::cout << "###--->  You should start thinking happy and positive thoughts since I asked\n###--->  for a positive number only.\n";
+                       std::cout << "###--->  You should start thinking happy and positive thoughts since I asked\n###--->  "
+									"for a positive number only.\n";
                    }
 
-                   std::cout << "###--->  I am ignoring your answer of " << UserGuessedNumber << " since you are ignoring my rules.\n###--->  I asked you to guess a number from " << MinNumber << " to " << MaxNumber << ".\n###--->  How hard can that be to follow?\n\n\n";
+                   std::cout << "###--->  I am ignoring your answer of " 
+							 << UserGuessedNumber 
+							 << " since you are ignoring my rules.\n###--->  I asked you to guess a number from " 
+							 << MinNumber 
+							 << " to " 
+							 << MaxNumber 
+							 << ".\n###--->  How hard can that be to follow?\n\n\n";
                }
 
            } while ((UserGuessedNumber < MinNumber) || (UserGuessedNumber > MaxNumber));
@@ -98,12 +130,16 @@ int main()
            {
                UserGuesses.push_back(UserGuessedNumber);
            } else {
-               std::cout << "\n\n###--->  You have already chosen the number " << UserGuessedNumber << ". Please try again\n\n\n";
+               std::cout << "\n\n###--->  You have already chosen the number " 
+						 << UserGuessedNumber 
+						 << ". Please try again\n\n\n";
            }
 
        } while (UserAlreadyGuessedThisNumber);
 
-       std::cout << "]  You chose the number " << UserGuessedNumber << " and ";
+       std::cout << "]  You chose the number " 
+				 << UserGuessedNumber 
+				 << " and ";
 
        if (UserGuessedNumber < SecretNumber)
        {
@@ -121,7 +157,8 @@ int main()
                 case 1:  std::cout << "you nailed it!\n]  You disarmed the treasure chest and recieved tons of gold coins!\n";
                          break;
 
-                case 2:  std::cout << "I can't believe you guessed it!\n]  I think you cheated and I'm not playing with you anymore!\n";
+                case 2:  std::cout << "I can't believe you guessed it!\n]  I think you cheated and I'm not playing with you "
+									  "anymore!\n";
                          break;
             }
 
@@ -131,10 +168,13 @@ int main()
 
                 switch(RandomMessage) {
 
-                    case 0:  std::cout << "\n\n###\n###  Whoa! That was a very close call! You guessed my number on your last try!\n###\n";
+                    case 0:  std::cout << "\n\n###\n###  Whoa! That was a very close call! You guessed my number on your "
+										  "last try!\n###\n";
                              break;
 
-                    case 1:  std::cout << "\n\n###\n###  I think you already knew my number and waited until the last moment just\n###  for thrill of the suspense, and to make everyone else sweat so you can\n###  have a great laugh at our expense.\n###\n";
+                    case 1:  std::cout << "\n\n###\n###  I think you already knew my number and waited until the last moment "
+										  "just\n###  for thrill of the suspense, and to make everyone else sweat so you can"
+										  "\n###  have a great laugh at our expense.\n###\n";
                              break;
                 }
             }
@@ -145,9 +185,11 @@ int main()
 
        if (GuessesLeft > 0)
        {
-          std::cout << "\n\n###  You have " << GuessesLeft << " more chance";
-          if (GuessesLeft > 1) std::cout << "s";
-          std::cout << " left to guess the number I am thinking of.\n\n\n";
+          std::cout << "\n\n###  You have " 
+					<< GuessesLeft 
+					<< " more chance" 
+					<< (GuessesLeft > 1 ? "s" : "")
+					<< " left to guess the number I am thinking of.\n\n\n";
        }
 
   } while (GuessesLeft > 0);
@@ -158,18 +200,20 @@ int main()
 
       switch(RandomMessage) {
 
-            case 0:  std::cout << "\n\n###\n###\n###  You did not guess the number I was thinking of within the " << MaxGuessesLeft << " guess";
-                     if(MaxGuessesLeft > 1) std::cout << "es";
-                     std::cout << "\n###  I gave you.\n###  Better luck next time!\n###\n###\n\n";
+            case 0:  std::cout << "\n\n###\n###\n###  You did not guess the number I was thinking of within the " 
+							   << MaxGuessesLeft 
+							   << " guess"
+							   << (MaxGuessesLeft > 1 ? "es" : "")
+							   << "\n###  I gave you.\n###  Better luck next time!\n###\n###\n\n";
                      break;
 
             case 1:  std::cout << "\n\n###\n###\n###  Well, you are not a winner this time. Please try again.\n###\n###\n\n";
                      break;
 
-            case 2:  std::cout << "\n\n###\n###\n###  Well, you failed to disarm the trap, and the treasure\n###  chest exploded disintegrating all the gold coins inside.\n###  Please try again.\n###\n###\n\n";
+            case 2:  std::cout << "\n\n###\n###\n###  Well, you failed to disarm the trap, and the treasure\n###  chest "
+								  "exploded disintegrating all the gold coins inside.\n###  Please try again.\n###\n###\n\n";
                      break;
       }
   }
-
-  return 0;
 }
+
